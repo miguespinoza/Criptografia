d65cb34a2d566c38f6eec6e231f06d0c378773ed
miguel <miguespinoza@hotmail.com>
2017-03-31 12:27:17 -0600

    multiplicacion funcionando

--- AES/multiplicacion.cpp
+++ AES/multiplicacion.cpp
@@ -23,30 +23,30 @@ void multiplicacion(BYTE a, BYTE b, BYTE *res){
 	BYTE polinomio (0x1B);
 	BYTE *resultados=(BYTE*)malloc(sizeof(BYTE)*8);
 	BYTE *suma=(BYTE*)malloc(sizeof(BYTE)*8);
-	resultados[0]=a; 
-	cout<<resultados[0].to_string()<<endl<<endl;
-	for (int i = 1; i < 7; i++)
+
+	//cout<<resultados[0].to_string()<<endl<<endl;
+	for (int i = 0; i < 7; i++)
 	{
-
-		if(a[7]==1){//se desbordara
-				a=((a<<1)^polinomio);
+		if(i!=0){
+			if(a[7]==1){//se desbordara
+					a=((a<<1)^polinomio);
+					resultados[i]=a;
+			}
+			else{
+				a=(a<<1);
 				resultados[i]=a;
-		}
-		else{
-			a=(a<<1);
-			resultados[i]=a;
-		}
-		cout<<resultados[i].to_string()<<endl<<endl;
-	}
-	*res=bitset<8> (0x00);
-
-	for(int i = 0;i<7;i++){
-		if(b[i]==1){
-			suma[i]=bitset<8> (0xFF);
+			}
 		}
 		else
+			resultados[0]=a; 
+		//cout<<resultados[i].to_string()<<endl<<endl;
+		if(b[i]==1)
+			suma[i]=bitset<8> (0xFF);
+		else
 			suma[i] =bitset<8> (0x00);
+
 	}
+	*res=bitset<8> (0x00);
 	*res=((resultados[0]&suma[0] )^(resultados[1]&suma[1] )^(resultados[2]&suma[2] )^(resultados[3]&suma[3] )^(resultados[4]&suma[4] )^(resultados[5]&suma[5] )^(resultados[6]&suma[6])^(resultados[7]&suma[7]));
 	cout<<"Resultado: "<< *res<<endl;
 
